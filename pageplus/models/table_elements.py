1dd590d5b931228ae9e10097512d4f763c421175
Stefan Weil <sw@weilnetz.de>
2024-01-28 14:25:53 +0100

    Remove unused import statement
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- pageplus/models/table_elements.py
+++ pageplus/models/table_elements.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import logging
 from dataclasses import dataclass, field
 from typing import Any, Optional
 
@@ -10,7 +9,7 @@ from pageplus.models.text_elements import TextRegion
 
 @dataclass
 class TableRegion(Region):
-    parent: None = field(default_factory=Any) # Page object
+    parent: None = field(default_factory=Any)  # Page object
     tablecells: list = field(default_factory=list)
     textlines: list = field(default_factory=list)
 
@@ -18,7 +17,7 @@ class TableRegion(Region):
         """
         Initializes the TableRegion by extracting TableCell elements and their text lines.
         """
-        self.tablecells = [TableCell(ele, self.ns, parent=self) \
+        self.tablecells = [TableCell(ele, self.ns, parent=self)
                            for ele in self.xml_element.iter(f"{{{self.ns}}}TableCell")]
         [self.textlines.append(tc.textlines) for tc in self.tablecells]
 
